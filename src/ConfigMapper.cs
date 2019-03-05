e1c3bf07fb2c84065d99cba4bfd82ef8192d7e26
MartinBerthold <MartinBerthold@NB-FC-215335>
2019-03-05 15:16:22 +0100

    change naming of connection

---
+++ src/ConfigMapper.cs
@@ -0,0 +1,11 @@
+﻿using Ser.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Q2g.HelperQlik
+{
+    //In the future use this Api without the dependence to Ser.Api.
+    public class ConnectionConfig : SerConnection { }
+}
